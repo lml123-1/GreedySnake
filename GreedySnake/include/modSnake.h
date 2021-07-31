a20036b28aa82c0590bd3cabbdb5419239a3f0c6
lml123-1 <1417769514@qq.com>
2021-07-31 21:41:56 +0800

    1

---
+++ GreedySnake/include/modSnake.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "snake.h"
+
+void modSnakeInit();
+void modSnakeRun();
+
---
+++ GreedySnake/src/modSnake.cpp
@@ -0,0 +1,12 @@
+#include "modSnake.h"
+#include "snake.h"
+
+void modSnakeInit()
+{
+    _MYSNAKEINFO.SnakeInit();
+}
+
+void modSnakeRun()
+{
+    _MYSNAKEINFO.SnakeRun();
+}
